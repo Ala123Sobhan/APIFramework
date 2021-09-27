08a1787edbab0b30b7a12a61b0f85a08a2ff5eec
Ala123Sobhan <ala123sobhan@gmail.com>
2021-09-27 12:21:26 -0400

    adding logging in separate file using request spec buildder

--- src/test/java/stepDef/addPlace.java
+++ src/test/java/stepDef/addPlace.java
@@ -17,6 +17,8 @@ import resources.TestData;
 import resources.Utils;
 
 import static io.restassured.RestAssured.given;
+
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,33 +30,34 @@ public class addPlace extends Utils {
     Response response;
 
     @Given("Add place payload is ready")
-    public void add_place_payload_is_ready() {
+    public void add_place_payload_is_ready() throws FileNotFoundException {
 
         TestData d = new TestData();
 
-
-
-         responseSpec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON)
-                .build();
-
-         res = given().log().all().spec(getRequestSpecification())
+        res = given().spec(getRequestSpecification())
                 .body(d.addPlace());
     }
+
     @When("user calls {string} with Post http request")
     public void user_calls_with_post_http_request(String string) {
-       response = res.when()
+
+        responseSpec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON)
+                .build();
+        response = res.when()
                 .post("/maps/api/place/add/json")
                 .then().spec(responseSpec).extract().response();
     }
+
     @Then("the API call returns status code {int}")
     public void the_api_call_returns_status_code(int stscode) {
         Assert.assertEquals(response.getStatusCode(), stscode);
     }
+
     @Then("the {string} in response body is {string}")
     public void the_in_response_body_is(String key, String value) {
-      String apiRes = response.asString();
+        String apiRes = response.asString();
         JsonPath js = new JsonPath(apiRes);
-       Assert.assertEquals( js.get(key), value);
+        Assert.assertEquals(js.get(key), value);
     }
 
 
